d6f2d392969f54fdb5bf3c5a033de6281b2df147
wangxiaobao <superwang9527@163.com>
2024-02-27 18:07:47 +0800

    feat: added exif info

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,17 +2,38 @@ import { useEffect, useRef, useState } from 'react';
 import { fabric } from 'fabric';
 import { loadImage } from '@/utils';
 import exifr from 'exifr';
+import canonLogo from '@/assets/images/canon.png';
+import fujifilmLogo from '@/assets/images/fujifilm.png'; // 富士
+import nikonLogo from '@/assets/images/nikon.png';
+import panasonicLogo from '@/assets/images/panasonic.png'; // 松下
+import sonyLogo from '@/assets/images/sony.png';
+
+const LOGOHEIGHT = 60;
+const MAXWIDTH = 1200;
+const MAXHEIGHT = 800;
+
+const logoMap: Record<string, string> = {
+  canon: canonLogo,
+  fujifilm: fujifilmLogo,
+  nikon: nikonLogo,
+  panasonic: panasonicLogo,
+  sony: sonyLogo,
+};
 
 const Index = () => {
   const fileRef = useRef<HTMLInputElement>(null);
-  const [canvas, setCanvas] = useState<fabric.Canvas>();
-  const [imgInfo, setImgInfo] = useState<{ file: File | null }>({ file: null });
+  const canvas = useRef<fabric.Canvas>();
+  const logoCanvas = useRef<fabric.Canvas>();
+  const [imgInfo, setImgInfo] = useState<{ file: File | null; exifInfo: any }>({
+    file: null,
+    exifInfo: {},
+  });
 
   useEffect(() => {
     const fabricCanvas = new fabric.Canvas('mainCanvas');
-    setCanvas(fabricCanvas);
-    fabricCanvas.setDimensions({ width: 800, height: 300 });
-
+    const fabricLogoCanvas = new fabric.Canvas('logoCanvas');
+    canvas.current = fabricCanvas;
+    logoCanvas.current = fabricLogoCanvas;
     if (!fileRef.current) {
       return;
     }
@@ -29,23 +50,85 @@ const Index = () => {
 
     reader.onload = async function (e) {
       const img = await loadImage(e.target?.result as string);
-      let scaleFactor = 1;
-      if (img.width! > 800) {
-        scaleFactor = 800 / img.width!;
+
+      if (img.width! > MAXWIDTH || img.height! > MAXHEIGHT) {
+        const scaleFactor = Math.min(
+          MAXWIDTH / img.width!,
+          MAXHEIGHT / img.height!
+        );
         img.scale(scaleFactor);
       }
-      const newWidth = img.width! * scaleFactor;
-      const newHeight = img.height! * scaleFactor + 30;
-      canvas!.setDimensions({ width: newWidth, height: newHeight });
-      setImgInfo({ file });
-      console.log('img', file);
-      canvas!.add(img);
+      const newWidth = img.width! * img.scaleX!;
+      const newHeight = img.height! * img.scaleY!;
+      canvas.current!.setDimensions({ width: newWidth, height: newHeight });
+      logoCanvas.current!.setDimensions({
+        width: newWidth,
+        height: LOGOHEIGHT,
+      });
+      console.log('img', file, img);
+      img.selectable = false;
+      canvas.current!.add(img);
+
+      let files = Array.from(fileRef.current!.files!);
+      let exifs = await Promise.all(
+        files.map((file) => exifr.parse(file, true))
+      );
+      console.log('---exifs', exifs);
+      setImgInfo({ file, exifInfo: exifs[0]! });
     };
 
-    let files = Array.from(fileRef.current!.files!);
-    let exifs = await Promise.all(files.map((file) => exifr.parse(file, true)));
-    console.log('---exifs', exifs);
     reader.readAsDataURL(file);
+  };
+
+  useEffect(() => {
+    if (!imgInfo.file) {
+      return;
+    }
+    renderEditContent();
+  }, [imgInfo]);
+
+  const renderEditContent = async () => {
+    console.log(111, imgInfo.exifInfo);
+    if (
+      !imgInfo.exifInfo?.Make ||
+      !logoMap[(imgInfo.exifInfo?.Make || '').toLocaleLowerCase()]
+    ) {
+      return;
+    }
+    console.log('imgInfo.exifInfo', imgInfo.exifInfo);
+    // const logoImg = await loadImage(
+    //   logoMap[(imgInfo.exifInfo?.Make || '').toLocaleLowerCase()]
+    // );
+    // logoImg.originX = 'center';
+    // logoImg.originY = 'center';
+    // logoImg.scale(0.2);
+    // logoImg.top = LOGOHEIGHT / 2;
+    // logoImg.left = Math.floor((logoImg.width! * logoImg.scaleX!) / 2);
+    // logoCanvas.current?.add(logoImg);
+
+    const modelText = new fabric.Text(imgInfo.exifInfo?.Model || '', {
+      fontSize: 20,
+      fill: '#333',
+      fontWeight: 'bold',
+    });
+    modelText.left = 0;
+    modelText.top = 0;
+
+    // logoCanvas.current?.add(modelText);
+
+    const LensModelText = new fabric.Text(imgInfo.exifInfo?.LensModel || '', {
+      fontSize: 16,
+      fill: '#666',
+      fontWeight: 'bold',
+    });
+    LensModelText.left = modelText.left;
+    LensModelText.top = modelText.top + modelText.height! + 8;
+    // logoCanvas.current?.add(LensModelText);
+    const leftGroup = new fabric.Group([modelText, LensModelText], {
+      left: 20,
+    });
+    leftGroup.top = (LOGOHEIGHT - leftGroup.height!) / 2;
+    logoCanvas.current?.add(leftGroup);
   };
 
   const uploadImg = () => {
@@ -64,7 +147,8 @@ const Index = () => {
         </button>
       </div>
       <div className="bg-white">
-        <canvas id="mainCanvas"></canvas>
+        <canvas id="mainCanvas" width={MAXWIDTH} height={300}></canvas>
+        <canvas id="logoCanvas" width={MAXWIDTH} height={LOGOHEIGHT}></canvas>
       </div>
     </div>
   );
